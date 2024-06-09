2d123219e459d8ff3e941216d9b960b5e14c5f10
Omar Elweshy <omarelweshy@gmail.com>
2024-06-10 00:09:24 +0300

    move db connection from router & add env

--- cmd/main.go
+++ cmd/main.go
@@ -2,17 +2,43 @@ package main
 
 import (
 	"log"
+	"os"
 
+	"github.com/joho/godotenv"
 	"github.com/omarelweshy/EcomMaster-user-service/internal/logger"
+	"github.com/omarelweshy/EcomMaster-user-service/internal/model"
 	"github.com/omarelweshy/EcomMaster-user-service/internal/router"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
 )
 
-func main() {
+func setupDB() (*gorm.DB, error) {
+	dbURL := os.Getenv("DATABASE_URL")
 
+	db, err := gorm.Open(postgres.Open(dbURL), &gorm.Config{})
+	if err != nil {
+		return nil, err
+	}
+	return db, nil
+}
+
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatalf("Error loading .env file")
+	}
+
+	port := os.Getenv("PORT")
 	logger.InitLogger()
 
-	r := router.SetupRouter()
-	if err := r.Run(":8000"); err != nil {
+	db, err := setupDB()
+	if err != nil {
+		log.Fatalf("Failed to connect to database: %v", err)
+	}
+	db.AutoMigrate(&model.User{})
+
+	r := router.SetupRouter(db)
+	if err := r.Run(":" + port); err != nil {
 		log.Fatal(err)
 	}
 }
--- internal/router/router.go
+++ internal/router/router.go
@@ -1,30 +1,18 @@
 package router
 
 import (
-	"log"
-
 	"github.com/gin-gonic/gin"
 	_ "github.com/omarelweshy/EcomMaster-user-service/docs"
 	"github.com/omarelweshy/EcomMaster-user-service/internal/handler"
 	"github.com/omarelweshy/EcomMaster-user-service/internal/middleware"
-	"github.com/omarelweshy/EcomMaster-user-service/internal/model"
 	"github.com/omarelweshy/EcomMaster-user-service/internal/repository"
 	"github.com/omarelweshy/EcomMaster-user-service/internal/service"
 	swaggerFiles "github.com/swaggo/files"
 	ginSwagger "github.com/swaggo/gin-swagger"
-
-	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
-func SetupRouter() *gin.Engine {
-	dsn := "host=localhost user=postgres password=omarelweshy dbname=ecommaster_user_service port=5432 sslmode=disable"
-	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	db.AutoMigrate(&model.User{})
+func SetupRouter(db *gorm.DB) *gin.Engine {
 	userRepository := &repository.UserRepository{DB: db}
 	userService := &service.UserService{Repo: *userRepository}
 	userHandler := &handler.UserHandler{UserService: userService}
